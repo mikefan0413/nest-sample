70fcf78945a467bbe4a087b51581a4546250c1b4
Mike Fan <sfan0413@gmail.com>
2022-10-23 09:51:35 -0400

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,9 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('hello/:term')
+  public sampleRoute(@Param('term') term: string): string {
+    return `your term: ${term}`;
   }
 }
